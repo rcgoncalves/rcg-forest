763b7e6ec19daf7772b7073272796ed60a126096
Rui Gonçalves <rgoncalves@di.uminho.pt>
2016-08-06 16:22:47 +0100

    Style improvements

--- functions.php
+++ functions.php
@@ -30,9 +30,9 @@ function rcg_forest_scripts_styles() {
 	if(is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
 	}
-	wp_enqueue_script('rcg-forest-navigation', get_template_directory_uri() . '/inc/navigation.js', array(), '20140816', true);
-	wp_enqueue_style('rcg-forest-style', get_stylesheet_uri(), array(), '20140816');
-	wp_enqueue_style('rcg-forest-ie', get_template_directory_uri() . '/inc/ie.css', array('rcg-forest-style'), '20140816');
+	wp_enqueue_script('rcg-forest-navigation', get_template_directory_uri() . '/inc/navigation.js', array(), '2.1.4', true);
+	wp_enqueue_style('rcg-forest-style', get_stylesheet_uri(), array(), '2.1.4');
+	wp_enqueue_style('rcg-forest-ie', get_template_directory_uri() . '/inc/ie.css', array('rcg-forest-style'), '2.1.4');
 	$wp_styles->add_data('rcg-forest-ie', 'conditional', 'lt IE 9');
 }
 add_action('wp_enqueue_scripts', 'rcg_forest_scripts_styles');
