03f58320b18b45c9aec63cb4b2a6a9f4d9ad0393
rcg <rcgoncalves.pt@gmail.com>
2020-06-11 21:14:46 +0200

    General improvements
    Add support for privacy link in footer.
    Add CSS for new calendar widget markup.
    Add call to `wp_body_open()`.
    Update static files cache busters parameters automatically from theme
    version number.
    Improve error handling for tags.

--- header.php
+++ header.php
@@ -12,18 +12,19 @@
 		<meta charset="<?php bloginfo('charset'); ?>" />
 		<meta name="viewport" content="width=device-width" />
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		<link rel="pingback" href="<?php echo esc_url(get_bloginfo('pingback_url')); ?>" />
 		<!--[if lt IE 9]>
-		<script src="<?php echo get_template_directory_uri(); ?>/inc/html5.js" type="text/javascript"></script>
+		<script src="<?php echo esc_url(get_template_directory_uri()); ?>/inc/html5.js" type="text/javascript"></script>
 		<![endif]-->
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
+		<?php wp_body_open(); ?>
 		<div id="global" class="hfeed base">
 			<div id="top1" class="site-top1">
 				<div class="site-top-center">
 					<header id="titlehead" class="site-header" role="banner">
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url('/')); ?>" title="<?php echo esc_attr( get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+						<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
 						<h2 class="site-description"><?php bloginfo('description'); ?></h2>
 					</header>
 				<?php echo get_search_form(false); ?>
@@ -43,7 +44,7 @@
 				?>
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<h3 class="menu-toggle"><?php _e('Menu', 'rcg-forest'); ?></h3>
-					<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'rcg-forest'); ?>"><?php _e('Skip to content', 'rcg-forest'); ?></a>
+					<a class="assistive-text" href="#content" title="<?php esc_attr_e('Skip to content', 'rcg-forest'); ?>"><?php _e('Skip to content', 'rcg-forest'); ?></a>
 					<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'nav-menu')); ?>
 				</nav>
 			</header>
