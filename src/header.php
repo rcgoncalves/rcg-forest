2eee2b76b594e818e7f4646e03dcd5f115f4df3b
rcg <rcgoncalves.pt@gmail.com>
2020-06-13 14:31:13 +0200

    Drop support for IE 7 and IE 8

--- src/header.php
+++ src/header.php
@@ -1,13 +1,5 @@
 <!DOCTYPE html>
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) | !(IE 8)  ]><!-->
 <html <?php language_attributes(); ?>>
-<!--<![endif]-->
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>" />
 		<meta name="viewport" content="width=device-width" />
