1c41bd7ffca4d4bef329168b312a35ff94c427bd
Rui Carlos Goncalves <rcgoncalves.pt@gmail.com>
2024-05-29 22:36:33 +0100

    Add code check action

--- src/404.php
+++ src/404.php
@@ -5,11 +5,11 @@ get_header();
 	<div id="content" role="main">
 		<article class="error404">
 			<header class="entry-header">
-				<h1 class="entry-title"><?php _e('Error 404: Page Not Found', 'rcg-forest'); ?></h1>
+				<h1 class="entry-title"><?php _e( 'Error 404: Page Not Found', 'rcg-forest' ); ?></h1>
 			</header>
 			<div class="entry-content">
-				<p><?php _e('We can not find what you are looking for.', 'rcg-forest'); ?></p>
-				<p><?php printf(__('Return <a href="%s">home</a>, or try search.', 'rcg-forest'), esc_url(get_home_url())); ?></p>
+				<p><?php _e( 'We can not find what you are looking for.', 'rcg-forest' ); ?></p>
+				<p><?php printf( __( 'Return <a href="%s">home</a>, or try search.', 'rcg-forest'), esc_url( get_home_url() ) ); ?></p>
 			</div>
 		</article>
 	</div>
